58871dc9f508297f81dbc403c5cc7fe5afdd0561
Daniel Hall <danielhall@improbable.io>
2018-10-30 18:56:28 +0000

    Updated starting point

--- Game/Source/ThirdPersonShooter/Characters/TPSCharacter.h
+++ Game/Source/ThirdPersonShooter/Characters/TPSCharacter.h
@@ -68,6 +68,8 @@ public:
 	// Returns the player's name, as specified on login.
 	FString GetPlayerName() const;
 
+	float TakeDamage(float Damage, const struct FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
 	FORCEINLINE float GetCurrentHealth() const
 	{
 		return CurrentHealth;
