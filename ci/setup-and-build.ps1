df9782955f4a87ee3e4675b16718a60ad9111375
patrick <patrickhansell@improbable.io>
2019-02-21 10:14:49 +0000

    Adding msbuild path

--- ci/setup-and-build.ps1
+++ ci/setup-and-build.ps1
@@ -67,7 +67,7 @@ pushd "UnrealEngine"
     Write-Log "Downloading the Unreal Engine artifacts from GCS"
     $gcs_unreal_location = "$($unreal_version).zip"
 
-    <#$gsu_proc = Start-Process -Wait -PassThru -NoNewWindow "gsutil" -ArgumentList @(`
+    $gsu_proc = Start-Process -Wait -PassThru -NoNewWindow "gsutil" -ArgumentList @(`
         "cp", `
         "gs://$($gcs_publish_bucket)/$($gcs_unreal_location)", `
         "$($unreal_version).zip" `
@@ -85,7 +85,7 @@ pushd "UnrealEngine"
     if ($zip_proc.ExitCode -ne 0) { 
         Write-Log "Failed to unzip Unreal Engine. Error: $($zip_proc.ExitCode)" 
         Throw "Failed to unzip Unreal Engine."  
-    }#> 
+    } 
 
     $unreal_path = "$($game_home)\UnrealEngine"
     Write-Log "Setting UNREAL_HOME environment variable to $($unreal_path)"
@@ -115,8 +115,10 @@ pushd "$($game_home)"
 
     Start-Event "set-up-gdk-plugin" "set-up-gdk-plugin-:windows:"
     pushd "Game/Plugins/UnrealGDK"
-        # Set the gdk_home variable for the GDK's setup script to use
+        # Set the required variables for the GDK's setup script to use
         $gdk_home = Convert-Path .
+        $msbuild_exe = "${env:ProgramFiles(x86)}\MSBuild\14.0\bin\MSBuild.exe"
+
         # Invoke the setup script
         &"$($game_home)\Game\Plugins\UnrealGDK\ci\setup-gdk.ps1"
     popd
