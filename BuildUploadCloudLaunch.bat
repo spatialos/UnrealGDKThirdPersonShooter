c0b9557aa3b6e92a49fd34efa2d935cbbe88075d
Joshua Huburn <joshuahuburn@improbable.io>
2019-02-26 18:44:44 +0000

    WIP scripts and workd

---
+++ BuildUploadCloudLaunch.bat
@@ -0,0 +1,5 @@
+call BuildWorkers.bat
+pushd spatial
+spatial cloud upload TPS_AAA_Dev --force
+spatial cloud launch TPS_AAA_Dev one_worker_test.json tps_aaa_dev --snapshot=snapshots/default.snapshot
+popd
