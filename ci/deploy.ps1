df69c49e6b0cec938df5c151a28f81d9362a6bcc
Patrick Hansell <patrickhansell@hotmail.co.uk>
2019-03-14 15:42:24 +0000

    Adding build annotation

--- ci/deploy.ps1
+++ ci/deploy.ps1
@@ -5,34 +5,43 @@ Start-Event "deploy-game" "build-gdk-third-person-shooter-:windows:"
     $deployment_name = "shooter_$($gdk_commit_hash)"
     $assembly_name = "$($deployment_name)_asm"
 
-pushd "spatial"
-    Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
-        "build", `
-        "build-config"
-    )
+    pushd "spatial"
+        Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
+            "build", `
+            "build-config"
+        )
 
-    Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
-        "prepare-for-run", `
-        "--log_level=debug"
-    )
+        Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
+            "prepare-for-run", `
+            "--log_level=debug"
+        )
 
-    Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
-        "cloud", `
-        "upload", `
-        "$assembly_name", `
-        "--log_level=debug", `
-        "--force"
-    )
+        Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
+            "cloud", `
+            "upload", `
+            "$assembly_name", `
+            "--log_level=debug", `
+            "--force"
+        )
 
-    Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
-        "cloud", `
-        "launch", `
-        "$assembly_name", `
-        "$deployment_launch_configuration", `
-        "$deployment_name", `
-        "--snapshot=$deployment_snapshot_path", `
-        "--cluster_region=$deployment_cluster_region", `
-        "--log_level=debug"
-    )
-popd
+        Start-Process -Wait -PassThru -NoNewWindow -FilePath "spatial" -ArgumentList @(`
+            "cloud", `
+            "launch", `
+            "$assembly_name", `
+            "$deployment_launch_configuration", `
+            "$deployment_name", `
+            "--snapshot=$deployment_snapshot_path", `
+            "--cluster_region=$deployment_cluster_region", `
+            "--log_level=debug"
+        ) | Tee-Object -FilePath "deploy.log"
+
+        $deployment_url = Select-String -Path "deploy.log" -Pattern '.*Console URL:(.*)\\n"'
+        if ($deployment_url)
+        {
+            buildkite-agent annotate --style "success" "Deployment URL: $deployment_url"
+        }
+        else{
+            buildkite-agent annotate --style "warning" "Could not parse deployment URL from launch log."
+        }
+    popd
 Finish-Event "deploy-game" "build-gdk-third-person-shooter-:windows:"
