816d7224bf68de9540d5942e4b0835107c723f81
Ben Naccarato <bennaccarato@improbable.io>
2019-04-05 11:00:40 +0100

    fix up header to match modifications

--- Game/Source/ThirdPersonShooter/BigGym/ActorCreatorBase.h
+++ Game/Source/ThirdPersonShooter/BigGym/ActorCreatorBase.h
@@ -15,14 +15,7 @@ public:
 	// Sets default values for this actor's properties
 	AActorCreatorBase();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 	UFUNCTION(BlueprintCallable, Category = "Actor Creator")
 	void CreateActors();
 
